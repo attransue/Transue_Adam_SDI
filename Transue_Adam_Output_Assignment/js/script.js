473eef510729549d1fe5a00028a6eb3e44467db2
attransue <adamtransue@Adams-MacBook-Pro.local>
2015-09-30 23:08:38 -0400

    2nd commit added variables

--- Transue_Adam_Output_Assignment/js/script.js
+++ Transue_Adam_Output_Assignment/js/script.js
@@ -5,7 +5,13 @@
  **/
 
 //Variables will go here
+var numberOfKids = "1";
+var daughtersName = "Drew";
+var placeOfEmployment = "Monroe County 911 center";
+var havePreviousProgramingExperiance = true;
 
 
 
 //outputs will reside here
+console.log("I have a total of " + numberOfKids "kids");
+
