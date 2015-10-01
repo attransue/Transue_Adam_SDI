2881135d3c7cf0951bed1cc5db27d306a83bb826
attransue <adamtransue@Adams-MacBook-Pro.local>
2015-09-30 23:28:59 -0400

    5th commit
    run the program and checked for errors. corrected all the errors that
    occurred at runtime

--- Transue_Adam_Output_Assignment/js/script.js
+++ Transue_Adam_Output_Assignment/js/script.js
@@ -8,13 +8,14 @@
 var numberOfKids = "1";  //Number of variable
 var daughtersName = "Drew";  // String variable
 var placeOfEmployment = "Monroe County 911 center";  // string variable
-var havePreviousProgramingExperiance = true;   // boolean variable
-
+var havePreviousProgramingExperiance = (true);   // boolean variable
+var dutysOfEmployment = "I work as a 911 dispatcher and call taker, answering the phone and dispatching the necessary authority\'s"
 
 
 //outputs will reside here
-console.log("I have a total of " + numberOfKids "kids.");
-console.log("My beautiful daughters name is " + daughtersName ".");
-console.log("I am currently employed by the " + placeOfEmployment ".");
-console.log("Do I have any previous programming experiance" + havePreviousProgramingExperiance);
+console.log("I have a total of " + numberOfKids  + " kids.");  // use of a number value
+console.log("My beautiful daughters name is " + daughtersName + "."); // use of a string value
+console.log("I am currently employed by the " + placeOfEmployment + "."); // use of a string value
+console.log("While at work my job duties include " + dutysOfEmployment + "."); // use of a string value
+console.log("Do I have any previous programming experiance? " + havePreviousProgramingExperiance); //use of a boolean value
 
