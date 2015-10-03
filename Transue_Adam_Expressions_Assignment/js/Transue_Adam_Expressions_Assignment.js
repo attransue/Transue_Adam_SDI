6eea843584985654bbdb9df238ff78ee3a10d3b5
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-03 13:46:34 -0400

    2nd Commit
    Add the variables and commented on what they do.

--- Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
+++ Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
@@ -7,7 +7,20 @@
  Expressions Assignment
  **/
 
+/**
+ This program will take user input for determined values and will calculate the friction loss through
+ a piece of fire hose. Which is determinate of the diameter of the hose and the gallons per minute of water and the
+ length of said hose.
+ **/
+
+
+
+
 // Variables Will Be Declared here.
+var lengthOfHose;  // Empty variable that will be assigned the user input value for the length of the hose
+var gallonsPerMinute;  // Empty variable that will be assigned the user input value for the GPM of water that is being flowed
+var diameterCoefficient;  // Empty variable that will be assigned the user input value for the Diameter of the Hose being Used
+var calculationsTable = []; // An array to be used to store the values of the user input
 
 
 
