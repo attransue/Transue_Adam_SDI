ba285728ceee868fb43a21c2c575d9ba259b7db3
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-03 19:25:36 -0400

    4th commit
    Added the user inputed variables to the array and commented out
    everything so far.

--- Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
+++ Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
@@ -24,10 +24,15 @@ var calculationsTable = []; // An array to be used to store the values of the us
 
 
 // User inputs will happen here.
-lengthOfHose = prompt("Please Enter the Total Length of the Hose Being Used.");
-gallonsPerMinute = prompt("Please Enter the Amount of Water Being Used in Gallons Per Minute.");
-diameterCoefficient = prompt("Using the Following Table Please Enter The Coefficient That represents the Diameter of hose being used. \n.75 inches = 1100 \n1.0 inches = 150 \n1.25 inches = 80 \n1.5 inches = 24 \n1.75 inches = 15.5 \n2.0 inches = 8 \n2.5 inches = 2 \n3.0 inches = 0.677 \n3.5 inches = 0.34 \n4.0 inches = 0.2 \n4.5 inches = 0.1 \n5.0 inches = 0.08 \n6.0 inches = 0.05");
+lengthOfHose = prompt("Please Enter the Total Length of the Hose Being Used.");  // Assigns the input value to lengthOfHose
+gallonsPerMinute = prompt("Please Enter the Amount of Water Being Used in Gallons Per Minute.");  // Assigns Value to gallonsPerMinute
+diameterCoefficient = prompt("Using the Following Table Please Enter The Coefficient That represents the Diameter of hose being used. \n.75 inches = 1100 \n1.0 inches = 150 \n1.25 inches = 80 \n1.5 inches = 24 \n1.75 inches = 15.5 \n2.0 inches = 8 \n2.5 inches = 2 \n3.0 inches = 0.677 \n3.5 inches = 0.34 \n4.0 inches = 0.2 \n4.5 inches = 0.1 \n5.0 inches = 0.08 \n6.0 inches = 0.05"); // Assigns value to diameterCoefficient
 
+
+//Variables will be assigned to the array here
+calculationsTable[0] = lengthOfHose; // assignment of the length of hose to the Array at index 0
+calculationsTable[1] = gallonsPerMinute;  //assignment of the gallons per minute to the Array at index 1
+calculationsTable[2] = diameterCoefficient;  // assignment of the diameter of hose coefficient to the Array at index 2
 
 
 //Math type stuff will happen here.
