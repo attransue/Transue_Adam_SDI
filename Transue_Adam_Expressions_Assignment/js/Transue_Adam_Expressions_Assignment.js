4c76137151585f35686d7df76b6513c07acaddca
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-03 13:38:47 -0400

    1st commit
    set up file structure and commented what the program will do.

---
+++ Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
@@ -0,0 +1,25 @@
+/**
+ * Created by adamtransue on 10/3/15.
+ */
+/**
+ Adam Transue
+ SDI 1510
+ Expressions Assignment
+ **/
+
+// Variables Will Be Declared here.
+
+
+
+
+// User inputs will happen here.
+
+
+
+//Math type stuff will happen here.
+
+
+
+// Outputs will happen here.
+
+
