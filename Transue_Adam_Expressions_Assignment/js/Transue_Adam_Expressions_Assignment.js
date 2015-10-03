11911b5e02847347a8036e5f036951ae3c662618
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-03 14:11:55 -0400

    3rd commit
    Add the user inputs section

--- Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
+++ Transue_Adam_Expressions_Assignment/js/Transue_Adam_Expressions_Assignment.js
@@ -23,9 +23,10 @@ var diameterCoefficient;  // Empty variable that will be assigned the user input
 var calculationsTable = []; // An array to be used to store the values of the user input
 
 
-
-
 // User inputs will happen here.
+lengthOfHose = prompt("Please Enter the Total Length of the Hose Being Used.");
+gallonsPerMinute = prompt("Please Enter the Amount of Water Being Used in Gallons Per Minute.");
+diameterCoefficient = prompt("Using the Following Table Please Enter The Coefficient That represents the Diameter of hose being used. \n.75 inches = 1100 \n1.0 inches = 150 \n1.25 inches = 80 \n1.5 inches = 24 \n1.75 inches = 15.5 \n2.0 inches = 8 \n2.5 inches = 2 \n3.0 inches = 0.677 \n3.5 inches = 0.34 \n4.0 inches = 0.2 \n4.5 inches = 0.1 \n5.0 inches = 0.08 \n6.0 inches = 0.05");
 
 
 
