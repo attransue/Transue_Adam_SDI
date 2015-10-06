6d8a61fac4a92e4e869dfd8882fb738eacc70a9b
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-06 16:54:08 -0400

    3rd Commit
    Added pizza problem part 2 commented out the first 2 problems

--- Transue_Adam_Expressions_Worksheet/js/Transue_Adam_Expressions_Worksheet.js
+++ Transue_Adam_Expressions_Worksheet/js/Transue_Adam_Expressions_Worksheet.js
@@ -8,7 +8,7 @@ ageInHumanYears = prompt("How old is sparky in human years?"); //Prompts the use
 ageInDogYears = ageInHumanYears * humanToDogYears; // Takes the user input and multiplies it by the constant of 7 years.
 console.log("Sparky is " + ageInDogYears + " years old in dog years"); // Prints out the results of the multiplication
 
-**/
+
 // Slices of Pizza Problem
 
 var slicesInPie; // Creates empty variable for how many times each pipe was sliced.
@@ -24,3 +24,23 @@ numberOfTotalSlices = slicesInPie * totalPizzasOrdered; //Calculates how many sl
 slicesPerPerson = numberOfTotalSlices / numberOfPartyGoers; // Figures out the total slices per person
 
 console.log("The total number of slices that was eaten by each party person was " + slicesPerPerson + " slices."); // Prints the results.
+
+ **/
+
+// Pizza Problem 2
+
+var slicesInPie; // Creates empty variable for how many times each pipe was sliced.
+var numberOfPartyGoers; // Creates a null variable for how many fun loving party goers are in attendance.
+var totalPizzasOrdered; // Creates a null value for how many pies are ordered
+var numberOfTotalSlices; // Variable to determine the total number of slices that have resulted from all the pizzas that were ordered.
+var slicesPerPerson; // Creates empty variable for the amount of slices eaten per person.
+var sparkysSlices; // creates variable for what sparky gets.
+
+slicesInPie = prompt("How many slices are contained in each pie that was orderd?"); // REcieves the value for sliceInPie Variable
+numberOfPartyGoers = prompt("How many fun loving people are attending this party?"); // Recieves the value for numberOfPartyGoers Variable
+totalPizzasOrdered = prompt("What is the total number of pizzas that were ordered.") // Receives the value for totalPizzasOrdered Variable
+numberOfTotalSlices = slicesInPie * totalPizzasOrdered; //Calculates how many slice of pizza are at the party all together.
+slicesPerPerson = numberOfTotalSlices / numberOfPartyGoers; // Figures out the total slices per person
+sparkysSlices = numberOfTotalSlices % numberOfPartyGoers; // Totals the amount of slices that sparky gets using the mod function
+
+console.log("The total number of slices that was eaten by sparky was " + sparkysSlices + " slices."); // Prints the results.
