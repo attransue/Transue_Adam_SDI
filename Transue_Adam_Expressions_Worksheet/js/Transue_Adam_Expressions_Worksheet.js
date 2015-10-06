df1455d3adfbe88255adec5c50ec37cd101f81d1
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-06 17:21:50 -0400

    4th commit
    Completed the grocery bill problem

--- Transue_Adam_Expressions_Worksheet/js/Transue_Adam_Expressions_Worksheet.js
+++ Transue_Adam_Expressions_Worksheet/js/Transue_Adam_Expressions_Worksheet.js
@@ -25,7 +25,7 @@ slicesPerPerson = numberOfTotalSlices / numberOfPartyGoers; // Figures out the t
 
 console.log("The total number of slices that was eaten by each party person was " + slicesPerPerson + " slices."); // Prints the results.
 
- **/
+
 
 // Pizza Problem 2
 
@@ -44,3 +44,35 @@ slicesPerPerson = numberOfTotalSlices / numberOfPartyGoers; // Figures out the t
 sparkysSlices = numberOfTotalSlices % numberOfPartyGoers; // Totals the amount of slices that sparky gets using the mod function
 
 console.log("The total number of slices that was eaten by sparky was " + sparkysSlices + " slices."); // Prints the results.
+
+ **/
+
+
+//Average Shopping Bill
+
+var billOne; // sets up variable for bill 1.
+var billTwo;  // sets up variable for bill 2.
+var billThree;   // sets up variable for bill 3.
+var billFour;   // sets up variable for bill 4.
+var billFive;   // sets up variable for bill 5.
+var tableOfBills = []; //Sets up array for storing the value of the bills.
+var totalOfBills; // sets up variable for the total of the bills.
+const numberOfBills = 5; // sets up the constant for the number of bills that will be used for the division side of average of the bills.
+var averageOfBills;  // sets up variable for average of the bills.
+
+billOne = prompt("What was the bill for the First Grocery Trip?"); // prompts for first bill
+billTwo = prompt("What was the bill for the Second Grocery Trip?"); // prompts for second bill
+billThree = prompt("What was the bill for the Third Grocery Trip?"); // prompts for third bill
+billFour = prompt("What was the bill for the Fourth Grocery Trip?");// prompts for fourth bill
+billFive = prompt("What was the bill for the Fifth Grocery Trip?"); // prompts for fifth bill
+
+tableOfBills[0] = Number(billOne); // assigns bill one to array
+tableOfBills[1] = Number(billTwo); // assigns bill two to array
+tableOfBills[2] = Number(billThree);  // assigns bill three to array
+tableOfBills[3] = Number(billFour); // assigns bill four to array
+tableOfBills[4] = Number(billFive); // assigns bill five to array
+
+totalOfBills = tableOfBills[0] + tableOfBills[1] + tableOfBills[2] + tableOfBills[3] + tableOfBills[4]; // figures out the total of all bills
+console.log("The total that was spent on Groceries the last five times is $" + totalOfBills + ".");// prints results of total of bills
+averageOfBills = totalOfBills / numberOfBills; // calcs the average of the bills
+console.log("The average of what was spent is $" + averageOfBills); // prints the average of the bills
