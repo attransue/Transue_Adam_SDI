d34bb392d7441759a8f58ef316f86ff3fcdabca4
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-07 17:12:36 -0400

    5th commit
    finished the assignment checked all the programs and they all function
    according to the assignment

--- Transue_Adam_Expressions_Worksheet/js/Transue_Adam_Expressions_Worksheet.js
+++ Transue_Adam_Expressions_Worksheet/js/Transue_Adam_Expressions_Worksheet.js
@@ -1,4 +1,4 @@
-/**
+
 // Dog years problem
 var ageInHumanYears;  // Creates the empty variable to be used to receive the age in human years
 const humanToDogYears = 7; // The constant that will be used to calculate the total
@@ -36,8 +36,8 @@ var numberOfTotalSlices; // Variable to determine the total number of slices tha
 var slicesPerPerson; // Creates empty variable for the amount of slices eaten per person.
 var sparkysSlices; // creates variable for what sparky gets.
 
-slicesInPie = prompt("How many slices are contained in each pie that was orderd?"); // REcieves the value for sliceInPie Variable
-numberOfPartyGoers = prompt("How many fun loving people are attending this party?"); // Recieves the value for numberOfPartyGoers Variable
+slicesInPie = prompt("How many slices are contained in each pie that was ordered?"); // Receives the value for sliceInPie Variable
+numberOfPartyGoers = prompt("How many fun loving people are attending this party?"); // Receives the value for numberOfPartyGoers Variable
 totalPizzasOrdered = prompt("What is the total number of pizzas that were ordered.") // Receives the value for totalPizzasOrdered Variable
 numberOfTotalSlices = slicesInPie * totalPizzasOrdered; //Calculates how many slice of pizza are at the party all together.
 slicesPerPerson = numberOfTotalSlices / numberOfPartyGoers; // Figures out the total slices per person
@@ -45,7 +45,7 @@ sparkysSlices = numberOfTotalSlices % numberOfPartyGoers; // Totals the amount o
 
 console.log("The total number of slices that was eaten by sparky was " + sparkysSlices + " slices."); // Prints the results.
 
- **/
+
 
 
 //Average Shopping Bill
@@ -76,3 +76,27 @@ totalOfBills = tableOfBills[0] + tableOfBills[1] + tableOfBills[2] + tableOfBill
 console.log("The total that was spent on Groceries the last five times is $" + totalOfBills + ".");// prints results of total of bills
 averageOfBills = totalOfBills / numberOfBills; // calcs the average of the bills
 console.log("The average of what was spent is $" + averageOfBills); // prints the average of the bills
+
+
+
+
+
+// Discount Program
+
+var itemName; // creates string variable to receive the input of the item name
+var originalPrice; // creates a variable for the original unaltered price
+var discountPercentage; // creates a variable for the percentage that the item will be discounted by
+var taxRate; // creates a variable for the tax percentage
+var discountedPriceNoTax; // creates a variable for the result of the original item minus the discount
+var discountedPriceWithTax; // creates a variable for the result of the original item minus the discount plus the sales tax
+
+itemName = prompt("Please enter the name of the item you would like to find the discounted rate and tax amounts for."); // receives the input for the item name
+originalPrice = prompt("Please enter the price for the item before any discounts have occurred."); // receives the inputted amount for the items original price
+console.log("So the " + itemName + " costs $" + originalPrice); // prints line showing the items name and original price
+discountPercentage = prompt("What is the discount that is going to be applied. PLease enter this as a number and do not include the percentage sign."); // receives the value of the pending discount percentage.
+discountPercentage = discountPercentage / 100; // converts the input percentage to a decimal for the impending math.
+taxRate = prompt("Unfortunately we all have to pay the man so please enter the appropriate sales tax rate. Note please do not include the percentage sign."); //recieves the sales tax rate.
+taxRate = taxRate / 100; // calculates the sales tax rate to a decimal to make it usable for the math functions
+discountedPriceNoTax = originalPrice - (originalPrice * discountPercentage); // figures out the price of the item at the discount price.
+discountedPriceWithTax = discountedPriceNoTax + (taxRate * discountedPriceNoTax); // calculates the items price after alldiscounts and with sales tax added in.
+console.log("Your " + itemName + " was originally $" + originalPrice + ", but after a " + discountPercentage * 100 + "% discount, it is now $" + discountedPriceNoTax + " without tax, and $" + discountedPriceWithTax + " with tax."); // prints the results in the proper format
