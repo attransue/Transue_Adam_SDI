f20ff3d248494e7a173c7e397880e912186f51ce
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-12 09:23:54 -0400

    finishing up lessons

---
+++ Conditional_Screencast/js/conditionals_Screencast.js
@@ -0,0 +1,20 @@
+/**
+ * Created by adamtransue on 10/11/15.
+ */
+/**
+ Adam Transue
+ SDI 1510
+ Conditionals Screencast
+ **/
+
+// *Video* Conditional Logic Concepts
+/**if(){
+
+}**/
+
+var kidHeight = 30;
+
+// if the  child is old enough, print to the console "you can ride
+if(kidHeight > 48){
+   console.log("You can indeed ride");
+}
