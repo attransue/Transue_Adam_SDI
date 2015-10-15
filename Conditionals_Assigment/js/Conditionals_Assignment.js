cf10376a93aa488dd5c281a4c463efa3628f7fb1
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-15 19:18:42 -0400

    final programming commit
    finished program completed fixed errors

--- Conditionals_Assigment/js/Conditionals_Assignment.js
+++ Conditionals_Assigment/js/Conditionals_Assignment.js
@@ -21,21 +21,16 @@ var billsCanBePaid = false;// boolean variable for weather or not monthly bills 
 // user input
 
 payRate = prompt("Please enter the rate at which your employer pays per hour");// prompts user for the input of hourly rate
-if (payRate === ""){// checks to see if input value was an empty string
+if (payRate === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
     payRate = prompt("Please enter the rate at which your employer pays per hour");// prompts user to re-input the value desired
-}else{// if the input value was anything other then an empty string
-    alert("Thank You Kindly");// alerts the user and thanks them for the input
-
 }
 payRate = Number(payRate);//cast user input into a number
 
 weeklyHours = prompt("Please enter how many hours you work per week");// prompts user for the input of total hours worked weekly
-if (weeklyHours === ""){// checks to see if input value was an empty string
+if (weeklyHours === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
     weeklyHours = prompt("Please enter how many hours you work per week");// prompts user to re-input the value desired
-}else {// if the input value was anything other then an empty string
-    alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
 weeklyHours = Number(weeklyHours);//cast user input into a number
 
@@ -43,17 +38,13 @@ freqOfPay = prompt("How many paychecks will you get this month?");// prompts use
 if (freqOfPay === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
     freqOfPay = prompt("How many paychecks will you get this month?");// prompts user to re-input the value desired
-}else {// if the input value was anything other then an empty string
-    alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
 freqOfPay = Number(freqOfPay);//cast user input into a number
 
-    phoneBill = prompt("Please enter the amount your carrier charges you to stay in contact with the world.");// prompts user for the input of phone bill cost
+phoneBill = prompt("Please enter the amount your carrier charges you to stay in contact with the world.");// prompts user for the input of phone bill cost
 if (phoneBill === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
     phoneBill = prompt("Please enter the amount your carrier charges you to stay in contact with the world.");// prompts user to re-input the value desired
-}else{// if the input value was anything other then an empty string
-    alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
 phoneBill = Number(phoneBill);//cast user input into a number
 
@@ -61,8 +52,6 @@ carNote = prompt("How much do you have to pay for your car each month?"); // pro
 if (carNote === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
     carNote = prompt("How much do you have to pay for your car each month?");// prompts user to re-input the value desired
-}else{// if the input value was anything other then an empty string
-    alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
 carNote = Number(carNote);//cast user input into a number
 
@@ -70,8 +59,6 @@ carIns = prompt("What is your car insurance monthly?");// prompts user for the i
 if (carIns === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
     carIns = prompt("What is your car insurance monthly?");// prompts user to re-input the value desired
-}else{// if the input value was anything other then an empty string
-    alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
 carIns = Number(carIns);//cast user input into a number
 
@@ -91,32 +78,24 @@ if(freqOfPay === 1){ // checks to see if the freqOfPay variable is = 1
     payCheck = (grossIncome * 4); // sets the paycheck variable = grossIncome * 4
 }
 
-billsCanBePaid = confirm("Your monthly income is $" + payCheck + " Your monthly bills total $" + billsAmount + " Is your monthly income more then your bills amount?  ('OK' for Yes 'Cancel' for No");
-console.log("bills value = " + billsCanBePaid);
-
+billsCanBePaid = confirm("Your monthly income is $" + payCheck + " Your monthly bills total $" + billsAmount + " Is your monthly income more then your bills amount?  ('OK' for Yes 'Cancel' for No");// returns boolean value for if bills is more than monthly total
 
 // outputs in here
 
 
+if(billsCanBePaid === true && (freqOfPay ===2 || freqOfPay === 4)){ // checks to see if billsCanBePaid is true and weather freqOfPay is 2 or 3
+    console.log("Based of the monthly income you will be able to pay all of your bills totaling $" + (billsAmount)); // prints the total of the monthly bills
+    console.log("You will only need to set aside $" + billsAmount / freqOfPay + " out of each paycheck to be able to succsesfully pay your bills");// prints how much of each check needs to be contributed
+} else {//if the values above are false
 
-if(billsCanBePaid === true && (freqOfPay ===2 || freqOfPay === 4)){
-    console.log("Based of the monthly income you will be able to pay all of your bills totaling $" + (billsAmount));
-    console.log("You will only need to set aside $" + billsAmount / freqOfPay + " out of each paycheck to be able to succsesfully pay your bills");
-} else{
-
-    if (billsCanBePaid >= carIns + carNote) {
-        console.log("You can pay your car loan and your car insurance totaling $" + (carIns + carNote));
-    } else if (billsCanBePaid >= carNote + phoneBill) {
-        console.log("You can pay your car payment and your phone bill totaling $" + carNote + phoneBill);
-    } else if (billsCanBePaid >= carIns + phoneBill) {
-        console.log("You can pay your car insurance and your phone bill totaling $" + carIns + phoneBill);
-    } else if (billsCanBePaid >= carIns && carNote && phoneBill) {
+    if (billsAmount >= carIns + carNote) { // checks to see if billsAmount is greater then the total of carIns and carNote
+        console.log("You can pay your car loan and your car insurance totaling $" + (carIns + carNote)); // prints out the total of the carIns and carNote
+    } else if (billsAmount >= carNote + phoneBill) {  // checks to see if billsAmount is greater then the total of carNote and phoneBill
+        console.log("You can pay your car payment and your phone bill totaling $" + carNote + phoneBill); // prints out the total of the phoneBill and carNote
+    } else if (billsAmount >= carIns + phoneBill) { // checks to see if billsAmount is greater then the total of carIns and phoneBill
+        console.log("You can pay your car insurance and your phone bill totaling $" + carIns + phoneBill); // prints out the total of the carIns and phoneBill
+    } else if (billsAmount >= carIns && billsAmount >= carNote && billsAmount >= phoneBill) { // checks to see if billsAmount is greater then any single bill
         console.log("You can pay either your phone bill, your car loan, or your car insurance. So please choose wisely!");
-    } else if (billsCanBePaid >= carIns && carNote) {
-        console.log("You can pay either your car loan, or your car insurance. So please choose wisely!");
-    } else if (billsCanBePaid >= carIns && phoneBill) {
-        console.log("You can pay either your phone bill or your car insurance. So please choose wisely!");
-    } else if (billsCanBePaid >= carNote && phoneBill) {
-        console.log("You can pay either your phone bill or your car loan. So please choose wisely!");
     }
 }
+(billsCanBePaid === true) ? console.log("You are working a sufficient job to pay your bills.") : console.log("You need to find another job to be able to pay your bills!");// checks to see if billsCanBePaid is true if so prints out the job is good if not prints out to find a new job.
