44ccb77a6074042431644790e8dee73b328b19a4
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-14 20:31:14 -0400

    math expressions added
    set up the beginning of the math expressions

--- Conditionals_Assigment/js/Conditionals_Assignment.js
+++ Conditionals_Assigment/js/Conditionals_Assignment.js
@@ -13,6 +13,9 @@ var freqOfPay;// sets a variable for how often pay is received
 var phoneBill; // sets a variable for how much the phone bill costs
 var carNote;  // sets a variable for how much the car payment is
 var carIns; // sets a variable for how much the car insurance costs
+var grossIncome; // sets a variable that totals the amount of gross income
+var payCheck; // sets a variable that receives the pay check total
+var billsCanBePaid; // boolean variable for weather or not monthly bills can be paid
 
 // user input
 
@@ -22,7 +25,10 @@ if (payRate === ""){// checks to see if input value was an empty string
     payRate = prompt("Please enter the rate at which your employer pays per hour");// prompts user to re-input the value desired
 }else{// if the input value was anything other then an empty string
     alert("Thank You Kindly");// alerts the user and thanks them for the input
+
 }
+payRate = Number(payRate);//cast user input into a number
+
 weeklyHours = prompt("Please enter how many hours you work per week");// prompts user for the input of total hours worked weekly
 if (weeklyHours === ""){// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
@@ -30,6 +36,8 @@ if (weeklyHours === ""){// checks to see if input value was an empty string
 }else {// if the input value was anything other then an empty string
     alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
+weeklyHours = Number(weeklyHours);//cast user input into a number
+
 freqOfPay = prompt("How many paychecks will you get this month?");// prompts user for the input of how often user gets paid
 if (freqOfPay === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
@@ -37,6 +45,8 @@ if (freqOfPay === "") {// checks to see if input value was an empty string
 }else {// if the input value was anything other then an empty string
     alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
+freqOfPay = Number(freqOfPay);//cast user input into a number
+
     phoneBill = prompt("Please enter the amount your carrier charges you to stay in contact with the world.");// prompts user for the input of phone bill cost
 if (phoneBill === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
@@ -44,6 +54,8 @@ if (phoneBill === "") {// checks to see if input value was an empty string
 }else{// if the input value was anything other then an empty string
     alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
+phoneBill = Number(phoneBill);//cast user input into a number
+
 carNote = prompt("How much do you have to pay for your car each month?"); // prompts user for the input of how much the car payment is
 if (carNote === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
@@ -51,6 +63,8 @@ if (carNote === "") {// checks to see if input value was an empty string
 }else{// if the input value was anything other then an empty string
     alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
+carNote = Number(carNote);//cast user input into a number
+
 carIns = prompt("What is your car insurance monthly?");// prompts user for the input of cost of car insurance
 if (carIns === "") {// checks to see if input value was an empty string
     alert("Please enter your input again there seems to have been an error receiving the input");// if input was an empty string alerts the user that no input was received
@@ -58,6 +72,24 @@ if (carIns === "") {// checks to see if input value was an empty string
 }else{// if the input value was anything other then an empty string
     alert("Thank You Kindly");// alerts the user and thanks them for the input
 }
+carIns = Number(carIns);//cast user input into a number
 
 //Math like expressions go in this section
 
+
+grossIncome = weeklyHours * payRate;// calculates the gross income amount based off of the input values
+
+if(freqOfPay === 1){ // checks to see if the freqOfPay variable is = 1
+    payCheck = grossIncome; // setts the payCheck variable = grossIncome
+} else if(freqOfPay === 2){ // checks to see if the freqOfPay variable is = 2
+    payCheck = grossIncome * 2; // sets the paycheck variable = grossIncome * 2
+} else if(freqOfPay === 4){ // checks to see if the freqOfPay variable is = 4
+    payCheck = grossIncome * 4; // sets the paycheck variable = grossIncome * 4
+}
+
+if((carIns + carNote + phoneBill) <= grossIncome){ //checks the totals of bills against grossIncome
+    billsCanBePaid = true; // sets the value of billsCanBePaid to true
+} else{ // if the if statement validates false
+    billsCanBePaid = false; // sets the value of billsCanBePaid to false
+}
+//if billsCanBePaid = false && billsCanBePaid <=
