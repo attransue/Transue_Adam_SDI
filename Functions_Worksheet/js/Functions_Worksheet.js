16b954963c840d7224df6bf4254013dfe44db097
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-20 22:18:26 -0400

    2nd commit
    added the sting problem

--- Functions_Worksheet/js/Functions_Worksheet.js
+++ Functions_Worksheet/js/Functions_Worksheet.js
@@ -11,3 +11,17 @@ function calcCirCircumference(){ // creates a function that takes one argument (
     return circumference; // returns the value of the math above
 }
 console.log(calcCirCircumference()); // prints out the value of the function
+
+
+
+// Stung!
+
+const stingsPerPound = 8.666666667; // constant for how man stings per pound to kill an animal
+
+function amountToKill(weight){ // creates a function the receives weight
+   var total = weight * stingsPerPound; // solves the math for amountToKill
+    return total; // returns the number of stings it requires
+}
+
+weight = prompt("What does the animal in question weigh?"); // prompts the user for the amount the animal weighs
+console.log("It will take a total of " + amountToKill(weight) + " stings to kill an animal weighing " + weight + " pounds"); // prints out the amount of stings it requires to kill an animal weighing "X" amount of pounds
