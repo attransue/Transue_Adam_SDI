5ef17f0685fc53543e49604582effd6ed9d5fe00
attransue <adamtransue@Adams-MBP.cmts.sth.ptd.net>
2015-10-22 23:27:56 -0400

    final commit
    final commit

--- Transue_Adam_Functions_Assignment/js/Assignment_Functions.js
+++ Transue_Adam_Functions_Assignment/js/Assignment_Functions.js
@@ -54,7 +54,6 @@ function userChoice(userInput){// creates a function for the verafication of the
 
 
 var pOrF = userChoice(choice); // creates a new variable for the results of the userChoice function
-floridaLotterNumbers = flLotteryDraw(35, 1);// assigns the value of the florida lottery function to the floridaLotterNumbers var
 
 if (pOrF ==="Powerball" || pOrF === "powerball" || pOrF ===  "Power ball" || pOrF ===  "power ball" || pOrF ===  "Power Ball") { // conditional to check if any one of several forms of "power ball" are input and then spits out the powerball numbers
     powerBallNumbers = powerBallDraw(59, 1); // assigns the value of the power ball function to the powerBallNumbers var
@@ -62,9 +61,8 @@ if (pOrF ==="Powerball" || pOrF === "powerball" || pOrF ===  "Power ball" || pOr
     console.log("Your Powerball Numbers are" + powerBallNumbers); // prints the results of power ball numbers
     console.log("And Your Powerplay number is " + powPlay); // prints the results of the power play number
 } else if (pOrF === "Florida Lottery" || pOrF ===  "florida lottery" || pOrF ===  "Florida" || pOrF ===  "lottery" || pOrF ===  "Lottery") { // conditional to check if any one of several forms of "Floirda Lottery" are input and then spits out the Florida Lottery numbers
+    floridaLotterNumbers = flLotteryDraw(35, 1);// assigns the value of the florida lottery function to the floridaLotterNumbers var
     console.log("The Lottery Numbers For the State of Florida are " + floridaLotterNumbers); // prints the variable for florida state lottery numbers
-} else { // for every thing else
-    alert("That is not a valid string please reload the page and try again!!!") // instructs the user to try again by reloading the page
 }
 
 
